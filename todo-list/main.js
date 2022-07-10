75954105f37c47696c439b92318fd64c42875d0c
ChyunKim <co1995@naver.com>
2022-07-10 12:13:19 +0900

    feat: Create document main.js

---
+++ todo-list/main.js
@@ -0,0 +1,53 @@
+var cnt = 1;
+var input = document.getElementById('task-input');
+var btn = document.getElementById('btn');
+var ul = document.getElementById('list');
+
+btn.addEventListener('click', function(e){
+  if(input.value.length==0){
+    alert("할일을 입력하세요!")
+  }
+  else{
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+
+    li.setAttribute("id","li"+cnt); 
+    li.innerHTML += "<button type='button' onclick='removeTodo("+cnt+")'>삭제</button>";
+    li.innerHTML += "<button type='button' onclick='completeTodo("+cnt+")'>완료</button>";
+    ul.appendChild(li);
+    cnt++;
+    input.value="";
+  
+  }
+})
+
+
+input.addEventListener('keypress', function(e){
+  if(e.key =='Enter'){
+    if(input.value.length==0){
+      alert("할일을 입력하세요!")
+    }else{
+      var li = document.createElement("li");
+      li.appendChild(document.createTextNode(input.value));
+      li.setAttribute("id","li"+cnt); 
+      li.innerHTML += "<button type='button' onclick='removeTodo("+cnt+")'>삭제</button>";
+      li.innerHTML += "<button type='button' onclick='completeTodo("+cnt+")'>완료</button>";
+      ul.appendChild(li);
+      cnt++;
+      input.value="";
+    }
+  }
+})
+
+
+function removeTodo(cnt){
+  var todolist = document.getElementById('li'+cnt)
+  ul.removeChild(todolist);
+}
+
+function completeTodo(cnt){
+  var todolist = document.getElementById('li'+cnt) 
+  todolist.style.textDecoration = "line-through"
+  todolist.style.textDecorationColor = "red"
+}
+
